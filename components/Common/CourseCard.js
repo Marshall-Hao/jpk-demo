aeab5c642af4c7beb5b04ea1a50da4646935e728
Marshall Chan <marshallchan666@outlook.com>
2022-07-02 14:54:34 +0800

    feat: courseCard teacher section implemented with map and slice,check css file for style ref

--- components/Common/CourseCard.js
+++ components/Common/CourseCard.js
@@ -1,9 +1,10 @@
 import { memo } from 'react'
 import Link from 'next/link'
 import s from './CourseCard.module.css'
+import icTeacher from 'public/img/teacher1.png'
 
 function CourseCard({ data = {} }) {
-  const { courseTitle = '', id, categoryName } = data
+  const { courseTitle = '', id, categoryName, courseTime, lessonNum, teacherList } = data
   return (
     <Link href="/course/details/[id]" as={`/course/details/;${id}`}>
       <a className={`${s.card} border-b-1px`}>
@@ -11,6 +12,31 @@ function CourseCard({ data = {} }) {
           <span className={s.categoryTag}>{categoryName}</span>
           <span className={s.title}>{courseTitle}</span>
         </h5>
+        <div className={s.time}>
+          <span>开课时间：&nbsp;&nbsp;{courseTime || '随到随学'}</span>
+          <span className={s.lessonNum}>{lessonNum}课时</span>
+        </div>
+        <div className={s.footer}>
+          <div className={s.teacherCont}>
+            {teacherList && teacherList.length > 0
+              ? teacherList.slice(0, 3).map((teacher, index) => {
+                  return (
+                    <div key={`teahcer-${index}`} className={s.teacherBox}>
+                      <div className={s.imgWrapper}>
+                        <img
+                          src={teacher.img || icTeacher}
+                          alt="teacher_avatar"
+                          className={s.avatar}
+                        />
+                        <div className={s.avatarBorder}></div>
+                      </div>
+                      <span>{teacher.name}</span>
+                    </div>
+                  )
+                })
+              : null}
+          </div>
+        </div>
       </a>
     </Link>
   )
