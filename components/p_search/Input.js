9746fb2d820007e8e7c195ffc8bb3040bee98050
Marshall Chan <marshallchan666@outlook.com>
2022-07-03 20:25:08 +0800

    feat: finish the handleKeyup and onChange logic, with some configured input event
    make the mobile analog keyboard more user friendly with adding type search

--- components/p_search/Input.js
+++ components/p_search/Input.js
@@ -14,6 +14,8 @@ export default function Input({
     const trimVal = searchVal.trim()
     setInputValue(e.target.value)
     if (!trimVal) {
+      // * 停止截流函数的timer 因为已经到历史页了 不需要在fetch suggest
+      fetchSuggest.cancel()
       showHistory()
       return
     }
@@ -24,6 +26,10 @@ export default function Input({
 
   const handleKeyUp = (e) => {
     if (e.keyCode !== 13 || !inputEl.current) return
+    // * 停止截流函数的timer 因为已经到结果页了 不需要在fetch suggest
+    const event = e || window.event
+    event.preventDefault() //* 阻止回车默认行为
+    fetchSuggest.cancel()
     const filteredVal = inputEl.current.value.trim()
     if (!filteredVal) {
       setInputValue('')
@@ -32,6 +38,21 @@ export default function Input({
     submitSearch(filteredVal)
     //*  blur,手机端收起虚拟键盘
     inputEl.current.blur()
+    //  * 禁止回车表单自动提交
+    return false
   }
-  return <input ref={inputEl} value={inputValue} onChange={handleChange} onKeyUp={handleKeyUp} />
+  return (
+    //  ! 让软键盘enter键显示搜索字样
+    <form action="">
+      <input
+        type="search"
+        ref={inputEl}
+        value={inputValue}
+        onChange={handleChange}
+        onKeyUp={handleKeyUp}
+      />
+      {/* 隐藏输入框，防止form自动提交 */}
+      <input type="text" style={{ display: 'none' }} />
+    </form>
+  )
 }
